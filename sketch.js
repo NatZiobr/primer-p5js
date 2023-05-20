1d3f8c051ba67c43a4f54941a54e314b7c2ba210
NatZiobr <ziobrowskinatali@gmail.com>
2023-05-20 16:41:27 -0300

    Creamos el primer commit del proyecto

---
+++ sketch.js
@@ -0,0 +1,79 @@
+
+function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background(255, 20, 50);
+  
+  
+    strokeWeight(37);
+    stroke(0, 0, 170);
+    point(300, 300);
+  
+    strokeWeight(18);
+    stroke(5, 5, 180);
+    noFill();
+    ellipse(300, 300, 60, 60);
+  
+    strokeWeight(17);
+    stroke(10, 10, 190);
+    noFill();
+    ellipse(300, 300, 100, 100);
+  
+    strokeWeight(16);
+    stroke(20, 20, 200);
+    noFill();
+    ellipse(300, 300, 140, 140);
+  
+    strokeWeight(15);
+    stroke(30, 30, 225);
+    noFill();
+    ellipse(300, 300, 180, 180);
+  
+    strokeWeight(14);
+    stroke(40, 40, 225);
+    noFill();
+    ellipse(300, 300, 220, 220);
+  
+    strokeWeight(13);
+    stroke(50, 50, 225);
+    noFill();
+    ellipse(300, 300, 260, 260);
+  
+    strokeWeight(12);
+    stroke(70, 70, 225);
+    noFill();
+    ellipse(300, 300, 300, 300);
+  
+    strokeWeight(9);
+    stroke(90, 90, 225);
+    noFill();
+    ellipse(300, 300, 340, 340);
+  
+    strokeWeight(6);
+    stroke(100, 100, 225);
+    noFill();
+    ellipse(300, 300, 380, 380);
+  
+    strokeWeight(5);
+    stroke(110, 110, 225);
+    noFill();
+    ellipse(300, 300, 420, 420);
+  
+    strokeWeight(4);
+    stroke(120, 120, 225);
+    noFill();
+    ellipse(300, 300, 460, 460);
+  
+    strokeWeight(3);
+    stroke(155, 155, 225);
+    noFill();
+    ellipse(300, 300, 500, 500);
+  
+    strokeWeight(2);
+    stroke(200, 200, 225);
+    noFill();
+    ellipse(300, 300, 540, 540);
+  }
+  
